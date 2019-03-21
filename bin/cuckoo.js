da0000b3c787650fb901061341ed5bf9233832a2
yemuguliunian <215822834@qq.com>
2019-03-21 21:46:28 +0800

    脚手架学习

--- bin/cuckoo.js
+++ bin/cuckoo.js
@@ -4,14 +4,13 @@ const semver = require('semver');
 
 const program = require('commander');
 const package = require('../package.json');
-const ora = require('ora');
 const chalk = require('chalk');
 
 const args = yParser(process.argv.slice(2));
-const script = args._[0];
+const script = args._[0] || 0;
 const project = args._[1];
 
-const generate = require('../src/generate.js');
+const run = require('../lib/run.js');
 
 program
   .version(package.version)
@@ -23,13 +22,13 @@ program.command('new (template)')
 program.parse(process.argv);
 
 if (!semver.satisfies(process.version, '>= 7.0.0')) {
-  	console.error(chalk.red('✘ The generator will only work with Node v8.0.0 and up!'));
+  	console.error(chalk.red('✘ The generator will only work with Node v7.0.0 and up!'));
   	process.exit(1);
 }
 
 switch(script) {
   case 'new' :
-    generate(project);
+    run(project);
     break;
   default :
     program.help();
